a86d2a38c3635008dfe04806edec539dd90f1603
raselsharif <raselsharif9@gmail.com>
2023-07-30 01:23:19 +0600

    added conditional

---
+++ conditional.js
@@ -0,0 +1,8 @@
+var iphonPrice = 95000;
+var myBudget = 9500;
+
+if (iphonPrice < myBudget){
+    console.log("Yes! I can buy iphone");
+}else{
+    console.log("Opps! I need to buy smart phone. Try next time.");
+}
