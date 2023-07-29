e10cba33f4e9a3e70dd96e5e9ff5b2bdf46502d0
raselsharif <raselsharif9@gmail.com>
2023-07-30 01:07:49 +0600

    added comparison

---
+++ comparison.js
@@ -0,0 +1,17 @@
+var num1 = 10;
+var num2 = 12;
+
+var result = num1 > num2;
+
+console.log(result);
+
+var result = num1 < num2;
+
+console.log(result);
+var result = num1 == num2;
+
+console.log(result);
+var num3 =12;
+var result = num3 == num2;
+
+console.log(result);
