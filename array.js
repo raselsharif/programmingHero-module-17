675d2314cb2bbae373ad672f91f994411a4f232d
raselsharif <raselsharif9@gmail.com>
2023-07-29 20:49:23 +0600

    added array

---
+++ array.js
@@ -0,0 +1,8 @@
+var friendsAge = [25, 27, 40, 30, 56, 58, 45, 12]
+var friendsName = ["jamal", "kamal"]
+
+console.log(friendsAge);
+console.log(friendsName);
+
+console.log(friendsAge.length);
+console.log(friendsName.length);
