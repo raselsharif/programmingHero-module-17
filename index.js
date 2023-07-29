6329688cc07ab92e37c6ef300f7cd36e26de0d64
raselsharif <raselsharif9@gmail.com>
2023-07-29 21:16:17 +0600

    added index

---
+++ index.js
@@ -0,0 +1,13 @@
+var number = [100,200,125,126,400,501];
+
+console.log(number);
+
+var index = number[1];
+console.log(index);
+
+number[1]=300;
+console.log(number);
+
+
+number=number.indexOf(400);
+console.log(number);
