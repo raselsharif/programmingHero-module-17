75f72f72870a075c423a4ec2a1d9fbd9762820a5
raselsharif <raselsharif9@gmail.com>
2023-07-30 01:55:55 +0600

    added multi contiond && ||

---
+++ mul-condition.js
@@ -0,0 +1,20 @@
+// and condition 
+
+var isGraduate = true;
+var salary = 20000;
+var hasCar = true;
+
+
+if (isGraduate == true && salary <= 20000 && hasCar == true) {
+    console.log("marry me");
+} else {
+    console.log("dure jao");
+}
+
+// or condition
+
+if (isGraduate == false || salary <=20000 || hasCar == false) {
+    console.log("marry me");
+} else {
+    console.log("dure jao");
+}
