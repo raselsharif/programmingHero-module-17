4eb106b656874990094f45091eb1ef6eeb67ca50
raselsharif <raselsharif9@gmail.com>
2023-07-30 00:48:07 +0600

    added push pop

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var number =[10,200,150,254,125]
+
+console.log(number);
+
+number.push(300);
+console.log(number);
+
+number.pop();
+console.log(number);
+
+number.unshift(400)
+console.log(number);
+
+number.shift();
+console.log(number);
