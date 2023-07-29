cdaa4ea753f35301e63a4cc8e3e41602d29b7f5d
raselsharif <raselsharif9@gmail.com>
2023-07-30 02:16:30 +0600

    added multi stage condtion

---
+++ multi-stage-condition.js
@@ -0,0 +1,14 @@
+var money = 9;
+var butterBon = 25;
+var biscuits = 15;
+var tea = 8;
+
+if (butterBon < money) {
+    console.log("eat butter ban");
+} else if (biscuits < money) {
+    console.log("eat biscuits");
+} else if (tea < money) {
+    console.log("drink tea");
+} else {
+    console.log("hawa khao");
+}
