08617aafae39b58e84101c93ab6339ba62e11ce6
Rasel Sharif <raselsharif9@gmail.com>
2023-07-30 17:37:01 +0600

    added task 02

---
+++ task/task02.js
@@ -0,0 +1,17 @@
+var totalNumber=60;
+
+if (totalNumber>=80){
+    if(totalNumber>100){
+        console.log("Your entered above 100");
+    }else{
+        console.log("A grade");
+    }
+}else if(totalNumber>=60){
+    console.log("B grade");
+}else if(totalNumber>=50){
+    console.log("C grade");
+}else if(totalNumber>=40){
+    console.log("D grade");
+}else{
+    console.log('F grade');
+}
