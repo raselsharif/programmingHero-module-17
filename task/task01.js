cf6ca6a1473a461fd75f52882b4729efe266077d
Rasel Sharif <raselsharif9@gmail.com>
2023-07-30 17:10:55 +0600

    added task 01

---
+++ task/task01.js
@@ -0,0 +1,16 @@
+var fruits = ['Apple', 'Banana', 'Orange'];
+console.log(fruits);
+
+// ==== indexing way one ===
+// fruits = fruits.indexOf('Banana');
+// console.log(fruits);
+
+// ==== indexing way one ===
+var index = fruits.indexOf('Banana');
+console.log(index);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push('Watermelon');
+console.log(fruits);
